787ea141db6725faae95e7ede07e148caf881405
A3therium <samkoseyhorner@gmail.com>
2023-11-08 18:10:38 +0000

    Refactored server class and added 404 page

--- framework/server-class.js
+++ framework/server-class.js
@@ -2,70 +2,81 @@ const http = require('node:http'); // set to https later
 const fs = require('node:fs');
 const path = require('node:path');
 
-function recursiveReadDir(filePath){
-    let data = {}
-    fs.readdirSync(filePath).forEach((file) => {
-        if (fs.statSync(filePath + file).isDirectory()){
-            data[file] = recursiveReadDir(filePath + file + '/');
-        } else {
-            data[file] = {content: fs.readFileSync(filePath + file), 
-                type: findMIMEType(path.extname(file).slice(1))};
-        }
-    });
-    return data
-} // Recursive function to scan and store all files in public
-
-function findMIMEType(fileExt){
-    switch (fileExt){
-        case 'js':
-            return 'text/javascript';
-        case 'html':
-            return 'text/html';
-        case 'css':
-            return 'text/css';
-        default:    
-            return null
-    }
-} // Converts file extensions into appropriate MIME type
-
-function recursiveObjSearch(obj, searchArray){
-    if (searchArray.length == 1){
-        return obj[searchArray[0]];
-    } else {
-        return recursiveObjSearch(obj[searchArray[0]], searchArray.slice(1));
-    }
-} // Recursive function to get data from publicFiles
-// Utility functions - NOT EXPORTED ^^^
-
 class Server {
     constructor (hostname, port, options){
         this.hostname = hostname;
         this.port = port
         this.options = options;
+    } // Server constructor
+
+    static recursiveReadDir(filePath){
+        let data = {}
+        fs.readdirSync(filePath).forEach((file) => {
+            if (fs.statSync(filePath + file).isDirectory()){
+                data[file] = Server.recursiveReadDir(filePath + file + '/');
+            } else {
+                data[file] = {content: fs.readFileSync(filePath + file), 
+                    type: Server.findMIMEType(path.extname(file).slice(1))};
+            }
+        });
+        return data
+    } // Recursive function to scan and store all files in public
+
+    static recursiveObjSearch(obj, searchArray){
+        if (searchArray.length == 1){
+            try {return obj[searchArray[0]];}
+            catch {return null;}
+        } else {
+            return Server.recursiveObjSearch(obj[searchArray[0]], searchArray.slice(1));
+        }
+    } // Recursive function to get data from publicFiles
+
+    static findMIMEType(fileExt){
+        switch (fileExt){
+            case 'js':
+                return 'text/javascript';
+            case 'html':
+                return 'text/html';
+            case 'css':
+                return 'text/css';
+            default:    
+                return null
+        }
+    } // Converts file extensions into appropriate MIME type
+
+    static getResource(res, resourceDirectory, url){
+        let urlArray = url.split('/').slice(1);
+        let tempResource = Server.recursiveObjSearch(resourceDirectory, urlArray);
+        if (!tempResource) {
+            let fofResource = Server.recursiveObjSearch(resourceDirectory, ['404-page.html']);
+            res.writeHead(404, {'Content-Type':'text/html'});
+            res.end(fofResource.content);
+        } else {
+            if (tempResource.type == null){
+                Server.error(res, 500);
+            } else {
+                res.writeHead(200, {'Content-Type':`${tempResource.type}`});
+                res.end(tempResource.content);
+            }
+        }
+    }
+
+    static error(res, code){
+        res.writeHead(code);
+        res.end();
     }
 
     run () {
-        this.publicFiles = recursiveReadDir('./public/');
+        this.publicFiles = Server.recursiveReadDir('./public/');
 
         this.server = http.createServer(this.options, (req, res) => {
             console.log({'method':req.method,'url':req.url});
             switch (req.method){
                 case 'GET':
-                    // Go to routing here
-                    // Below is all temp.
-                    let urlArray = req.url.split('/');
-                    urlArray = urlArray.slice(1);
-                    if (urlArray[0] == 'favicon.ico'){
-                        res.writeHead(404);
-                        res.end('Error 404 - resource not found')
-                    } else {
-                        res.writeHead(200, {'Content-Type':`${recursiveObjSearch(this.publicFiles, urlArray).type}`});
-                        res.end(recursiveObjSearch(this.publicFiles, urlArray).content);
-                    }
+                    Server.getResource(res, this.publicFiles, req.url);
                     break;
                 default:
-                    res.writeHead(405);
-                    res.end('Error 405 - method not allowed')
+                    Server.error(res, 405);
                     break;
             }
         });
