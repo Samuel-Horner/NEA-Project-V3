24a2577a31959fc67560305e04b1c5ddbf6c66f6
A3therium <samkoseyhorner@gmail.com>
2023-11-15 11:42:30 +0000

    added db init to server.js

--- database/init.js
+++ database/init.js
@@ -1,33 +1,37 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database(__dirname + '/dev.db');
+function initDB() {
+    const db = new sqlite3.Database(__dirname + '/dev.db');
 
-try {
-   db.serialize(() => {
-        db.run(`CREATE TABLE IF NOT EXISTS accountTbl (
-                accountID INT NOT NULL UNIQUE, 
-                username TEXT NOT NULL,
-                password TEXT NOT NULL, 
-                PRIMARY KEY (accountID));`, errorCallback);
-        db.run(`CREATE TABLE IF NOT EXISTS projectTbl (
-                projectID INT NOT NULL UNIQUE,
-                accountID INT NOT NULL,
-                projectName TEXT,
-                PRIMARY KEY (projectID),
-                FOREIGN KEY (accountID) REFERENCES accountTbl(accountID));`, errorCallback);
-        db.run(`CREATE TABLE IF NOT EXISTS contentTbl (
-                projectID INT NOT NULL,
-                type INT NOT NULL,
-                content TEXT,
-                PRIMARY KEY (projectID, type),
-                FOREIGN KEY (projectID) REFERENCES projectTbl(projectID));`, errorCallback);
-    }); // Creates tables 
-} catch {}
+    try {
+        db.serialize(() => {
+                db.run(`CREATE TABLE IF NOT EXISTS accountTbl (
+                        accountID INT NOT NULL UNIQUE, 
+                        username TEXT NOT NULL,
+                        password TEXT NOT NULL, 
+                        PRIMARY KEY (accountID));`, errorCallback);
+                db.run(`CREATE TABLE IF NOT EXISTS projectTbl (
+                        projectID INT NOT NULL UNIQUE,
+                        accountID INT NOT NULL,
+                        projectName TEXT,
+                        PRIMARY KEY (projectID),
+                        FOREIGN KEY (accountID) REFERENCES accountTbl(accountID));`, errorCallback);
+                db.run(`CREATE TABLE IF NOT EXISTS contentTbl (
+                        projectID INT NOT NULL,
+                        type INT NOT NULL,
+                        content TEXT,
+                        PRIMARY KEY (projectID, type),
+                        FOREIGN KEY (projectID) REFERENCES projectTbl(projectID));`, errorCallback);
+            }); // Creates tables 
+    } catch {}
 
-db.close()
+    db.close();
+}    
 
 function errorCallback(err){
     if (err){
         console.log(err);
     }
 }
+
+module.exports = {initDB};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Server = require('./framework/server-class'); // Framework class
+require('./database/init').initDB(); // Initialises DB
 
 let mainServer = new Server.Server('localhost','8080',{});
 mainServer.run();
@@ -6,6 +7,6 @@ mainServer.run();
 /* TODO -
 1. Implement proper routing (making use of imported functions?) [server-class.js] --DONE--
 2. Add error handling so server doesnt crash on faulty request [server-class.js]
-3. Implement a proper ORM [new files]
-4. Connect ORM to frontend [new files & frontend]
+3. Implement a proper DBMS [new files]
+4. Connect DBMS to frontend [new files & frontend]
 */
