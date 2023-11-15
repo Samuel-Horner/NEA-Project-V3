08d548d96f3c727e1dc872eab3bb323dda9f3d25
A3therium <samkoseyhorner@gmail.com>
2023-11-15 11:21:03 +0000

    finalised init.js

--- database/init.js
+++ database/init.js
@@ -1,33 +1,30 @@
 const sqlite3 = require('sqlite3').verbose();
-const sqlite = require('sqlite');
-const crypto = require('node:crypto');
 
 const db = new sqlite3.Database(__dirname + '/dev.db');
 
-// WARNING - Running this code will reset the databasess
+try {
+   db.serialize(() => {
+        db.run(`CREATE TABLE IF NOT EXISTS accountTbl (
+                accountID INT NOT NULL UNIQUE, 
+                username TEXT NOT NULL,
+                password TEXT NOT NULL, 
+                PRIMARY KEY (accountID));`, errorCallback);
+        db.run(`CREATE TABLE IF NOT EXISTS projectTbl (
+                projectID INT NOT NULL UNIQUE,
+                accountID INT NOT NULL,
+                projectName TEXT,
+                PRIMARY KEY (projectID),
+                FOREIGN KEY (accountID) REFERENCES accountTbl(accountID));`, errorCallback);
+        db.run(`CREATE TABLE IF NOT EXISTS contentTbl (
+                projectID INT NOT NULL,
+                type INT NOT NULL,
+                content TEXT,
+                PRIMARY KEY (projectID, type),
+                FOREIGN KEY (projectID) REFERENCES projectTbl(projectID));`, errorCallback);
+    }); // Creates tables 
+} catch {}
 
-db.serialize(() => {
-    try {db.run('DROP TABLE devTable;',errorCallback);} catch {} // Attempts to drop the table if it exists
-    db.run(`CREATE TABLE devTable (
-                rowID INT NOT NULL UNIQUE, 
-                someText TEXT, 
-                PRIMARY KEY (rowID));`);
-    for (i = 0; i < 100; i++){
-        let text = crypto.randomBytes(20).toString('hex');
-        db.run('INSERT INTO devTable VALUES (?, ?);', [i, text], errorCallback);
-    }
-    
-    db.each('SELECT rowID, someText FROM devTable', (err, row) => {
-        console.log(`${row.rowID} ${row.someText}`)
-    });
-}); // Creates tables
-
-// sqlite.open({
-//     filename: '/tmp/database.db',
-//     driver: sqlite3.Database
-//   }).then((db) => {
-//     // do your thing
-//   })
+db.close()
 
 function errorCallback(err){
     if (err){
