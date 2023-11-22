936a5068b31861384f0dcaefc9bedc0c61df4da9
A3therium <samkoseyhorner@gmail.com>
2023-11-22 10:09:57 +0000

    updated timeline

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const Server = require('./framework/server-class'); // Framework class
 
 let mainServer = new Server.Server('localhost','8080',{});
-require('./database/init.js').initDB().then((result) => {
+require('./database/init.js').initDB().then(() => {
     mainServer.openDB();
     mainServer.run();
 }); // Initialises DB and runs server
 
 /* TODO -
 1. Implement proper routing (making use of imported functions?) [server-class.js] --DONE--
-2. Add error handling so server doesnt crash on faulty request [server-class.js]
-3. Implement a proper DBMS [new files]
-4. Connect DBMS to frontend [new files & frontend]
+2. Add error handling so server doesnt crash on faulty request [server-class.js] -- N/A --
+3. Implement a proper DBMS [new files] -- 20% --
+4. Connect DBMS to frontend [new files & frontend] -- 20% --
 */
