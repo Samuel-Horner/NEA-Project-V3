56e738d91e5bb2963803d8b486de3577d9201694
A3therium <samkoseyhorner@gmail.com>
2023-12-17 18:33:35 +0000

    commented db-mgmt.js

--- database/db-mgmt.js
+++ database/db-mgmt.js
@@ -10,7 +10,8 @@ class dbManager {
         }).then((res) => {
                 res.on('trace', (data) => {
                     console.log(data);
-                });
+                }); // For debuging, prints any SQL statements recieved by the
+                    // DB to the console.
                 res.exec('PRAGMA foreign_keys = ON;'); // Enables foreign keys
                 return res;
         }).catch((err) => {
@@ -24,15 +25,15 @@ class dbManager {
     }
     async _dbExec(sql, params) {
         return await (await this.db).run(sql, params);
-    }
+    } // Runs some SQL statement and DOES NOT return result
 
     async _dbGet(sql, params){
         return await (await this.db).get(sql, params);
-    }
+    } // Runs some SQL statement and returns the first result row
 
     async _dbAll(sql, params){
         return await (await this.db).all(sql, params);
-    }
+    } // Runs some SQL statement and returns all result rows
 } // Wrapper class for db operations
 
 module.exports = {dbManager};
